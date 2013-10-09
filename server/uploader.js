1a49050a87d4ddea82e9e5adbaeeb92aef54deae
Sylvain Gizard <sylvain.gizard@gmail.com>
2013-10-09 17:49:18 +0200

    Fix broken upload...

--- server/uploader.js
+++ server/uploader.js
@@ -5,13 +5,15 @@ var readFileSync = Meteor._wrapAsync(fs.readFile);
 var tmpDir = process.env.TMP || process.env.TMPDIR || process.env.TEMP || "/tmp" || process.cwd();
 
 SmartFile.allow = function (options){
+    var baseName = stripExtension(options.fileName);
     Perseid.colls.albums.prePhotoInsertCheck({
-        name: stripExtension(options.fileName),
+        name: baseName,
         albumId: options.albumId
     });
 
     //Force photo to be stored within album directory
     options.path = options.albumId;
+    options.name = baseName;
 
     return true;
 };
@@ -19,35 +21,23 @@ SmartFile.allow = function (options){
 SmartFile.onUpload = function (result, options){
     Perseid.colls.photos.insert({
         albumId: options.albumId,
-        name: stripExtension(options.fileName),
+        name: options.name,
         date: new Date()
     });
 };
-
-function stripExtension(fileName){
-    var extension = path.extname(fileName);
-    return path.basename(fileName, extension);
-}
 
 SmartFile.onUploadFail = function (result, options){
     console.log("SmartFile upload failed: ", result);
 };
 
-function writeBufferToTempFile(buffer, fileName){
-    var tmpFile = path.resolve(tmpDir, fileName);
-    writeFileSync(tmpFile, buffer);
-    return tmpFile;
-}
-
 SmartFile.onIncomingFile = function (data, options){
     var tmpFile = writeBufferToTempFile(data, options.fileName);
     data = null; //discard the original buffer for GCing
 
-
     var uploadQueue = [tmpFile];
 
     [256, 512, 1024].forEach(function(size){
-        var resizedFile = path.resolve(tmpDir, baseName + "-" + size + ".jpg");
+        var resizedFile = path.resolve(tmpDir, options.name + "-" + size + ".jpg");
 
         Imagemagick.resize({
             srcPath: tmpFile,
@@ -78,4 +68,15 @@ function populate(dst, src) {
         if (!dst[prop]) dst[prop] = src[prop];
     }
     return dst;
+}
+
+function stripExtension(fileName){
+    var extension = path.extname(fileName);
+    return path.basename(fileName, extension);
+}
+
+function writeBufferToTempFile(buffer, fileName){
+    var tmpFile = path.resolve(tmpDir, fileName);
+    writeFileSync(tmpFile, buffer);
+    return tmpFile;
 }
