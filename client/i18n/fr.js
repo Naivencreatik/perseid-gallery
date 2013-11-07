a8487e79ab8987d1e2f5330d678619d78162d5e3
Sylvain Gizard <sylvain.gizard@gmail.com>
2013-11-07 14:36:52 +0000

    Bootstrapify header

--- client/i18n/fr.js
+++ client/i18n/fr.js
@@ -1,7 +1,7 @@
 i18n.map("fr", {
     "admin.albums.add": "Ajouter un album",
     "admin.posts.add": "Ajouter un article",
-    "admin.photos.add.file": "Ajouter une image",
+    "admin.photos.add.file": "Ajouter images",
     "admin.photos.add.youtube": "Ajouter video YouTube",
     "uploads.progress": "Envoi en cours",
     "album.dropfile": "Deposez vos images",
--- client/views/admin/header.js
+++ client/views/admin/header.js
@@ -1,29 +1,23 @@
 Template.adminAlbumListActions.events({
-    "click a": function (event, template){
-        event.preventDefault();
+    "click button": function (event, template){
         var albumName = prompt("Nom du nouvel album?");
         Meteor.call("album.add", albumName);
     }
 });
 
 Template.adminAddImage.events({
-    "click a": function (event, template){
-        event.preventDefault();
+    "click button": function (event, template){
         template.find("input").click();
     },
 
     "change input": function(event, template){
-        var input = event.target;
-        
+        var input = event.target;        
         Perseid.colls.albums.upload(this._id, input.files);
-
-        input.form.reset();
     }
 });
 
 Template.adminAddYoutube.events({
-    "click a": function (event, template){
-        event.preventDefault();
+    "click button": function (event, template){
         var youtubeUrl = prompt("Adresse/URL Youtube?");
         Meteor.call("photo.add.youtube", this._id, youtubeUrl);
     }
